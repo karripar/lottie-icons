5b491a7cb9b42dd5f83b6d62ad6f4bcb25bfcc45
Karri Partanen <karripar@km-d557-mim2-06.pk.amk.root>
2025-05-02 15:20:16 +0300

    something

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,6 +1,38 @@
-import { Link, Outlet } from 'react-router';
+import { Link, Outlet } from "react-router";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import taloData from "../assets/lottie/talo.json";
+import hamppariData from "../assets/lottie/hampurilainen.json";
+import { useEffect, useRef, useState } from "react";
 
 const Layout = () => {
+  const homeRef = useRef<LottieRefCurrentProps>(null);
+  const hamppariRef = useRef<LottieRefCurrentProps>(null);
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const hoverHandler = () => {
+    if (!homeRef.current) return;
+    homeRef.current.play();
+  };
+
+  const completeHandler = () => {
+    if (!homeRef.current) return;
+    homeRef.current.goToAndStop(0, true);
+  };
+
+  const menuHandler = () => {
+    setMenuToggle(!menuToggle);
+  };
+
+  useEffect(() => {
+    if (!hamppariRef.current) return;
+    if (menuToggle) {
+      hamppariRef.current.setDirection(1);
+    } else {
+      hamppariRef.current.setDirection(-1);
+    }
+    hamppariRef.current.play();
+  }, [menuToggle]);
+
   return (
     <div className="m-auto h-full w-11/12">
       <nav className="absolute right-0 flex flex-col-reverse items-end justify-end lg:relative lg:block lg:flex-row">
@@ -18,11 +50,25 @@ const Layout = () => {
           ease-in-out
           lg:flex
           lg:opacity-100
+          ${menuToggle ? "opacity-100" : "opacity-0"}
           `}
         >
           <li>
-            <Link className="block p-4 text-center  hover:bg-slate-300" to="/">
-              Home
+            <Link
+              className="block p-4 text-center  hover:bg-slate-300"
+              to="/"
+              onMouseEnter={hoverHandler}
+            >
+              <div className="flex items-baseline">
+                Home
+                <Lottie
+                  lottieRef={homeRef}
+                  animationData={taloData}
+                  loop={false}
+                  autoplay={false}
+                  onComplete={completeHandler}
+                />
+              </div>
             </Link>
           </li>
           <li>
@@ -35,7 +81,15 @@ const Layout = () => {
           </li>
         </ul>
         <div className="m-4 h-8 w-8 sm:block lg:hidden">
-          <button>Menu</button>
+          <div className="flex items-center justify-center">
+            <Lottie
+              lottieRef={hamppariRef}
+              animationData={hamppariData}
+              loop={false}
+              autoplay={false}
+              onClick={menuHandler}
+            />
+          </div>
         </div>
       </nav>
       <main className="">
